0e29f85d7a901b03118f64aa2cdb3fce4c1637b7
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-25 03:10:17 -0300

    chore: Update custom button

--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -14,8 +14,10 @@ class CustomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(200, 56),
+        minimumSize: const Size(236, 80),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        foregroundColor: Colors.black,
+        textStyle: const TextStyle(fontSize: 24),
       ),
       onPressed: actionOfButton,
       child: Text(typeOfAction),
