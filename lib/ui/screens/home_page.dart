d871f2305b0b5a23785b4b41d45db1fafc38d76e
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-25 03:10:41 -0300

    chore: Update home page

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cashpeak/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Hello world'));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomButton(typeOfAction: 'Registrar Gasto', actionOfButton: () {}),
+          const SizedBox(height: 32),
+          CustomButton(
+            typeOfAction: 'Registrar Ingreso',
+            actionOfButton: () {},
+          ),
+          const SizedBox(height: 32),
+          CustomButton(typeOfAction: 'Registros', actionOfButton: () {}),
+        ],
+      ),
+    );
   }
 }
