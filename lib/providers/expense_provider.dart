ec3724ea33fd13c0cb1975d3132c1573660309eb
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 18:36:59 -0300

    feat: add remove expense method and update expense provider

--- lib/providers/expense_provider.dart
+++ lib/providers/expense_provider.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 class ExpenseProvider extends ChangeNotifier {
   final List<Expense> _expenses = [
     Expense(date: DateTime(2022, 3, 14), name: 'tienda', expenseAmount: 100),
+    Expense(
+      date: DateTime(2022, 3, 14),
+      name: 'super mercado',
+      expenseAmount: 100,
+    ),
+    Expense(date: DateTime(2022, 3, 14), name: 'mencanico', expenseAmount: 100),
   ];
 
   List<Expense> get expenses => List.unmodifiable(_expenses);
@@ -11,5 +17,17 @@ class ExpenseProvider extends ChangeNotifier {
   void addExpense(Expense newExpense) {
     _expenses.add(newExpense);
     notifyListeners();
+  }
+
+  void removeExpense(Expense expenseToRemove, String name) {
+    _expenses.removeWhere((expenseToRemove) => expenseToRemove.name == name);
+    notifyListeners();
+  }
+
+  int get totalExpenseAmount {
+    return _expenses.fold<int>(
+      0,
+      (total, expense) => total + expense.expenseAmount,
+    );
   }
 }
