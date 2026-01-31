0bbd42d3272f33581031329c9e8935653047e443
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 16:16:34 -0300

    feat: create records screen

--- lib/ui/screens/records_screen.dart
+++ lib/ui/screens/records_screen.dart
@@ -5,6 +5,14 @@ class RecordsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      // body: ,
+    );
   }
 }
