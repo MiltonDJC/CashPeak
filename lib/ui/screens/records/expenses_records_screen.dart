93abb3875c4a6466c89fa3bd66510ea18bbe87ac
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 18:37:29 -0300

    feat: create expenses records screen

--- lib/ui/screens/records/expenses_records_screen.dart
+++ lib/ui/screens/records/expenses_records_screen.dart
@@ -1,18 +1,93 @@
+import 'package:cashpeak/providers/expense_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ExpensesRecordsScreen extends StatelessWidget {
   const ExpensesRecordsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final expenseProvider = context.watch<ExpenseProvider>();
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           onPressed: () => Navigator.pop(context),
           icon: const Icon(Icons.arrow_back),
         ),
-        title: const Text('Gastos registrados'),
+        title: const Text('Ingresos registrados'),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: expenseProvider.expenses.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            subtitle: Card(
+              shape: const RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          'Fecha: ${expenseProvider.expenses[index].date.day}/${expenseProvider.expenses[index].date.month}/${expenseProvider.expenses[index].date.year}\nNombre: ${expenseProvider.expenses[index].name.toUpperCase()}\nMonto: \$${expenseProvider.expenses[index].expenseAmount}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          expenseProvider.removeExpense(
+                            expenseProvider.expenses[index],
+                            expenseProvider.expenses[index].name,
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.delete_forever,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 50),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(top: BorderSide(color: Colors.grey)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Total de gastos:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '-\$${expenseProvider.totalExpenseAmount}',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
