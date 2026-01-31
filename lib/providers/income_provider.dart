64df661412a2d4d58ab876227e30b891bdac9e0b
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 18:26:05 -0300

    feat: add remove income method

--- lib/providers/income_provider.dart
+++ lib/providers/income_provider.dart
@@ -4,14 +4,23 @@ import 'package:flutter/material.dart';
 class IncomeProvider extends ChangeNotifier {
   final List<Income> _incomes = [
     Income(date: DateTime(2022, 3, 14), name: 'tienda', incomeAmount: 100),
-    Income(date: DateTime(2022, 3, 14), name: 'tienda', incomeAmount: 100),
-    Income(date: DateTime(2022, 3, 14), name: 'tienda', incomeAmount: 100),
+    Income(
+      date: DateTime(2022, 3, 14),
+      name: 'super mercado',
+      incomeAmount: 100,
+    ),
+    Income(date: DateTime(2022, 3, 14), name: 'mencanico', incomeAmount: 100),
   ];
 
   List<Income> get incomes => List.unmodifiable(_incomes);
 
   void addIncome(Income newIncome) {
     _incomes.add(newIncome);
+    notifyListeners();
+  }
+
+  void removeIncome(Income incomeToRemove, String name) {
+    _incomes.removeWhere((incomeToRemove) => incomeToRemove.name == name);
     notifyListeners();
   }
 
