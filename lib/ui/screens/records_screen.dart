a779dd66f4385ab9e1eb28917c0c806f847144e9
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 16:46:21 -0300

    chore: add navigation for income and expenses records screens

--- lib/ui/screens/records_screen.dart
+++ lib/ui/screens/records_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cashpeak/ui/screens/records/expenses_records_screen.dart';
+import 'package:cashpeak/ui/screens/records/income_records_screen.dart';
 import 'package:cashpeak/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +23,12 @@ class RecordsScreen extends StatelessWidget {
               width: 224,
               height: 80,
               child: CustomButton(
-                actionOfButton: () {},
+                actionOfButton: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const IncomeRecordsScreen(),
+                  ),
+                ),
                 typeOfAction: 'Registros de ingresos',
                 buttonBackgroundColor: Colors.green,
               ),
@@ -31,7 +38,12 @@ class RecordsScreen extends StatelessWidget {
               width: 224,
               height: 80,
               child: CustomButton(
-                actionOfButton: () {},
+                actionOfButton: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const ExpensesRecordsScreen(),
+                  ),
+                ),
                 typeOfAction: 'Registros de gastos',
                 buttonBackgroundColor: Colors.red,
               ),
