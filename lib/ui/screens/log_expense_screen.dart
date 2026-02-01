3b87b5b2f96c13ecb23aa8f6b31e17077ac031d0
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 23:04:59 -0300

    feat: add log expense screen

---
+++ lib/ui/screens/log_expense_screen.dart
@@ -0,0 +1,26 @@
+import 'package:cashpeak/ui/widgets/custom_expense_form.dart';
+import 'package:flutter/material.dart';
+
+class LogExpenseScreen extends StatefulWidget {
+  const LogExpenseScreen({super.key});
+
+  @override
+  State<LogExpenseScreen> createState() => LogExpenseScreenState();
+}
+
+class LogExpenseScreenState extends State<LogExpenseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text('Registrar gasto'),
+        centerTitle: true,
+      ),
+      body: const CustomExpenseForm(),
+    );
+  }
+}
