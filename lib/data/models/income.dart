a1cdc380ccd3cebc853a6580eedd291acf449a80
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 21:08:42 -0300

    Revert "refactor: add index for income and expenses models"
    This reverts commit 96c3a6a05b350e672e23c199d992a2f0ed77bae4.

--- lib/data/models/income.dart
+++ lib/data/models/income.dart
@@ -1,12 +1,6 @@
 class Income {
-  Income({
-    required this.incomeIndex,
-    required this.date,
-    required this.name,
-    required this.incomeAmount,
-  });
+  Income({required this.date, required this.name, required this.incomeAmount});
 
-  final int incomeIndex;
   final DateTime date;
   final String name;
   final int incomeAmount;
