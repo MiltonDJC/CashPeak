96c3a6a05b350e672e23c199d992a2f0ed77bae4
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 21:05:28 -0300

    refactor: add index for income and expenses models

--- lib/data/models/income.dart
+++ lib/data/models/income.dart
@@ -1,6 +1,12 @@
 class Income {
-  Income({required this.date, required this.name, required this.incomeAmount});
+  Income({
+    required this.incomeIndex,
+    required this.date,
+    required this.name,
+    required this.incomeAmount,
+  });
 
+  final int incomeIndex;
   final DateTime date;
   final String name;
   final int incomeAmount;
