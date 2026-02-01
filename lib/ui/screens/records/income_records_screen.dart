a8cbda10f58536cb14421ac75e8fcb9344beda6b
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 22:17:22 -0300

    feat: update custom form

--- lib/ui/screens/records/income_records_screen.dart
+++ lib/ui/screens/records/income_records_screen.dart
@@ -49,7 +49,7 @@ class IncomeRecordsScreen extends StatelessWidget {
                         onPressed: () {
                           incomeProvider.removeIncome(
                             incomeProvider.incomes[index],
-                            incomeProvider.incomes[index].incomeIndex,
+                            incomeProvider.incomes[index].incomeIndex!,
                           );
                         },
                         icon: const Icon(
