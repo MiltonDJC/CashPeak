0ff1ae621bfad0044a67d1505f22de117f07ce57
MiltonDJC <miltonaxe2001@gmail.com>
2026-01-31 21:07:21 -0300

    feat: update expense provider and remove expense method

--- lib/providers/income_provider.dart
+++ lib/providers/income_provider.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 
 class IncomeProvider extends ChangeNotifier {
   final List<Income> _incomes = [
-    Income(date: DateTime(2022, 3, 14), name: 'tienda', incomeAmount: 100),
     Income(
+      incomeIndex: 0,
+      date: DateTime(2022, 3, 14),
+      name: 'tienda',
+      incomeAmount: 100,
+    ),
+    Income(
+      incomeIndex: 1,
       date: DateTime(2022, 3, 14),
       name: 'super mercado',
       incomeAmount: 100,
     ),
-    Income(date: DateTime(2022, 3, 14), name: 'mencanico', incomeAmount: 100),
+    Income(
+      incomeIndex: 2,
+      date: DateTime(2022, 3, 14),
+      name: 'mencanico',
+      incomeAmount: 100,
+    ),
   ];
 
   List<Income> get incomes => List.unmodifiable(_incomes);
@@ -19,8 +30,10 @@ class IncomeProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void removeIncome(Income incomeToRemove, String name) {
-    _incomes.removeWhere((incomeToRemove) => incomeToRemove.name == name);
+  void removeIncome(Income incomeToRemove, int index) {
+    _incomes.removeWhere(
+      (incomeToRemove) => incomeToRemove.incomeIndex == index,
+    );
     notifyListeners();
   }
 
