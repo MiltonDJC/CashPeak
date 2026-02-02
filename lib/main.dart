12aa6afc43b879ee64978bf5d7aee45b4f949844
MiltonDJC <miltonaxe2001@gmail.com>
2026-02-02 17:49:43 -0300

    feat: initialize hive

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:cashpeak/providers/expense_provider.dart';
 import 'package:cashpeak/providers/income_provider.dart';
 import 'package:cashpeak/ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
   runApp(
     MultiProvider(
       providers: [
